a1bdd5bdb089ccc41cad30c28de45a97f49a490d
Luca Nicolas Baello <luca.baello@gmail.com>
2026-07-03 18:59:13 -0300

    Main.java y pom.xml tenia declarado el paquete de otros tp corrijo

--- src/main/java/ar/edu/unahur/obj2/energia/Main.java
+++ src/main/java/ar/edu/unahur/obj2/energia/Main.java
@@ -1,6 +1,7 @@
-package ar.edu.unahur.obj2.banco;
+package ar.edu.unahur.obj2.energia;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Sistema de Gestión de Energía Inteligente (SGEI)");
     }
 }
